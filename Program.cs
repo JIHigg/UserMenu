053f2635b101a7b48aa3c1d1d3c5384edcd29429
higginsdude@gmail.com <higginsdude@gmail.com>
2021-01-21 09:45:10 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UserMenu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
