bbc38dbecdb1c80e049d7c845d3cf6d2f09667f3
higginsdude@gmail.com <higginsdude@gmail.com>
2021-01-21 10:57:56 +0100

    Added Notes for instructions of branches

--- Program.cs
+++ Program.cs
@@ -6,7 +6,20 @@ namespace UserMenu
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //A while branch that holds the menu open
+
+            //Add an if branch for Menu option 1
+                //Check age and calculate ticket prices
+                //Display group size and cost of tickets
+            
+            //Add an if branch for Menu option 2
+                //Ask user for string input
+                //Display string ten times on same line
+
+            //Add an if branch for Menu option 3
+                //Ask user for a sentence
+                //Split sentence at the spaces
+                //Display third word of sentence
         }
     }
 }
